80e1b20317a16fad6ba0a9c2f4b95930af4af273
PARISOT Romain <romainparisot.pro@gmail.com>
2025-01-28 14:32:21 +0100

    feat: Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Hero from "@/components/Hero/Hero";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <section className="sectionHero">
+        <Hero />
+      </section>
+    </div>
+  );
 }
